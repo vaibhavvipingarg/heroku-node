f593b9975d9525b166de8c13ad818c1ef3d93c4e
Vaibhav Garg <vaibhav.garg@salesforce.com>
2020-06-25 18:20:18 -0700

    temp changes

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    app = express();
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
+console.log('Using limit: ', myLimit);
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+app.use(bodyParser.json({limit: myLimit}));
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+app.all('*', function (req, res, next) {
 
-// set the home page route
-app.get('/', function(req, res) {
+    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
 
-	// ejs render automatically looks in the views folder
-	res.render('index');
+    if (req.method === 'OPTIONS') {
+        // CORS Preflight
+        res.send();
+    } else {
+        // var targetURL = req.header('Target-URL');
+        // if (!targetURL) {
+        //     res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+        //     return;
+        // }
+        request({ url: 'https://na44.stmfa.stm.salesforce.com/services/data/v50.0/wave/folders', method: "GET", json: req.body, headers: {'Authorization': 'Bearer 00DRM000000GySC!AQkAQNG9EuYL5YfyaQmDYpZ4zUupRd8ML_CeVhIosp7Mb6nIquS3g33uD_SMSaUkgbqjNcHl9rHsF.r8mMXPpq.1a0KiQHMM', "Cookie": "BrowserId=fESh3bcvEeqVIqtpX5pgxQ"} },
+            function (error, response, body) {
+                console.log(error);
+                if (error) {
+                    console.error('error: ' + response.statusCode)
+                }
+               console.log(body);
+            }).pipe(res);
+    }
 });
 
-app.listen(port, function() {
-	console.log('Our app is running on http://localhost:' + port);
+app.set('port', process.env.PORT || 3001);
+
+app.listen(app.get('port'), function () {
+    console.log('Proxy server listening on port ' + app.get('port'));
 });
